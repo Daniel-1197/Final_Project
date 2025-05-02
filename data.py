4c8a51592099dc7eba0d934d141f9b2b0b9d8c9b
Daniel Prem <danielprem1197@gmail.com>
2025-05-02 14:01:31 +0530

    Delete data.py

--- data.py
+++
@@ -1,5 +0,0 @@
-class Data:
-    URL = "https://www.saucedemo.com"
-    DASHBOARD_URL = "https://www.saucedemo.com/inventory.html"
-    EXCEL_FILE = "E:\\Python\\capstoneProject\\test_datas.xlsx"
-    SHEET_NUMBER = "Sheet1"
